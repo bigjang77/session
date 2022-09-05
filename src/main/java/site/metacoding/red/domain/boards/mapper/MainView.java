3153adf6a4ea8e5817eb6bffb6258f0d3152f2b3
bigjang77 <dhex2004@gmail.com>
2022-09-05 11:59:38 +0900

    목록보기 완료

---
+++ src/main/java/site/metacoding/red/domain/boards/mapper/MainView.java
@@ -0,0 +1,12 @@
+package site.metacoding.red.domain.boards.mapper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MainView {//뷰를위한 데이터
+	private Integer id;
+	private String title;
+	private String username;
+}
--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -1,8 +1,11 @@
 package site.metacoding.red.web;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.red.domain.boards.Boards;
 import site.metacoding.red.domain.boards.BoardsDao;
+import site.metacoding.red.domain.boards.mapper.MainView;
 import site.metacoding.red.domain.users.Users;
 import site.metacoding.red.web.dto.request.boards.WriteDto;
 
@@ -22,16 +26,8 @@ public class BoardsController {
 	// @PostMapping("/boards/{id}/delete")
 	// @PostMapping("/boards/{id}/update")
 
-	@PostMapping("/boards")
+	@PostMapping("/boards")//글쓰기
 	public String writeBoards(WriteDto writeDto) {
-		//1번 세션에 접근해서 세션값을 확인한다.그때 Users로 다운캐스팅한다. 키값은 principal로 한다.
-		
-		//2번 principal null인지 확인하고 null이면 loginForm 리다이렉션해준다.
-		
-		//3번 BoardsDao에 접근해서 insert 메서드를 호출한다.
-		//조건:dto를 entity로 변환해서 인수로 담아준다.
-		//조건:entity에는 세션의 principal에 getid가 필요하다.
-		
 		Users principal = (Users) session.getAttribute("principal");
 
 		if (principal == null) {
@@ -42,19 +38,23 @@ public class BoardsController {
 		return "redirect:/";// 메인페이지로 리턴, 본코드
 	}
 
-	@GetMapping({ "/", "/boards" })
-	public String getBoardList() {
+
+	@GetMapping({ "/", "/boards" })//글전체목록보기
+	public String getBoardList(Model model) {
+		List<MainView>boardsList = boardsDao.findAll();
+		model.addAttribute("boardsList",boardsList);
 		return "boards/main";
 	}
 
-	@GetMapping("/boards/{id}")
-	public String getBoardList(@PathVariable Integer id) {
+	@GetMapping("/boards/{id}")//글한개보기
+	public String getBoardList(@PathVariable Integer id, Model model) {
+		model.addAttribute("boards",boardsDao.findById(id));
 		return "boards/detail";
 	}
 
-	@GetMapping("/boards/writeForm")
+	@GetMapping("/boards/writeForm")//글쓰기전 인증하기
 	public String writeForm() {
-		Users principal = (Users) session.getAttribute("principal");// 글쓰기 페이지는 권한이 필요없다 인증만 필요하다
+		Users principal = (Users)session.getAttribute("principal");// 글쓰기 페이지는 권한이 필요없다 인증만 필요하다
 		if (principal == null) {
 			return "redirect:/loginForm";
 		}
